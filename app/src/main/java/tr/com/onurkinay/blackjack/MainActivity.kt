5d599e0afb3081ea80ff546d43dbd3841d548a42
onur <onur.kinay@stu.fsm.edu.tr>
2019-06-08 01:02:21 +0300

    First commit for app

---
+++ app/src/main/java/tr/com/onurkinay/blackjack/MainActivity.kt
@@ -0,0 +1,46 @@
+package tr.com.onurkinay.blackjack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import kotlinx.android.synthetic.main.activity_main.*
+
+import kotlinx.android.synthetic.main.bet_dialog.view.*
+import java.util.Arrays
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+
+    val cards = arrayOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+    var bet: Int = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+// Enter bet!!
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.bet_dialog, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Enter bet!")
+        //show dialog
+        val  mAlertDialog = mBuilder.show()
+        //login button click of custom layout
+        mDialogView.dialogLoginBtn.setOnClickListener {
+            //dismiss dialog
+            mAlertDialog.dismiss()
+            //get text from EditTexts of custom layout
+            bet = mDialogView.dialogNameEt.text.toString().toInt()
+            dealer.text = bet.toString()
+            //set the input text in TextView
+
+        }
+        //cancel button click of custom layout
+        mDialogView.dialogCancelBtn.setOnClickListener {
+            exitProcess(-1)
+            mAlertDialog.dismiss()
+        }
+// !Enter bet
+    }
+
+}
