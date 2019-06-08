6a753ee2d1ab716a9c70a5a99407b4348745d879
onur <onur.kinay@stu.fsm.edu.tr>
2019-06-08 23:46:47 +0300

    Added bet

--- app/src/main/java/tr/com/onurkinay/blackjack/MainActivity.kt
+++ app/src/main/java/tr/com/onurkinay/blackjack/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         hit.setOnClickListener {
             hit()
         }
-        stand.setOnClickListener{
+        stand.setOnClickListener {
             stand()
         }
         double_b.setOnClickListener {
@@ -47,16 +47,18 @@ class MainActivity : AppCompatActivity() {
             .setView(mDialogView)
             .setTitle("Enter bet!")
         //show dialog
-        val  mAlertDialog = mBuilder.show()
+        val mAlertDialog = mBuilder.show()
+        mDialogView.walletStatus.text = "You have $" + wallet
         //login button click of custom layout
         mDialogView.dialogLoginBtn.setOnClickListener {
             //dismiss dialog
-            if (mDialogView.dialogBetEt.text.toString() != "") {
+            if (mDialogView.dialogBetEt.text.toString() != "" && wallet >= mDialogView.dialogBetEt.text.toString().toInt()) {
                 //get text from EditTexts of custom layout
                 bet = mDialogView.dialogBetEt.text.toString().toInt()
+                wallet -= bet
                 mAlertDialog.dismiss()
                 start_a_game()
-            }else{
+            } else {
 
             }
 
@@ -69,9 +71,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("SetTextI18n")
-    fun start_a_game(){
-        dealer_cards = mutableListOf( random_card(), random_card() )
-        player_cards = mutableListOf( random_card(), random_card() )
+    fun start_a_game() {
+        if (wallet < bet * 2) double_b.isEnabled = false
+        dealer_cards = mutableListOf(random_card(), random_card())
+        player_cards = mutableListOf(random_card(), random_card())
 
 
         dc_w = 0
@@ -79,37 +82,37 @@ class MainActivity : AppCompatActivity() {
 
         val pl_worth: Int by lazy {
             var worth: Int = 0
-            for (i in player_cards){
+            for (i in player_cards) {
                 worth += give_worth_of_the_card(i)
             }
             worth
         }
-        val dc_worth: Int by lazy{
+        val dc_worth: Int by lazy {
             var worth: Int = 0
-            for (i in dealer_cards){
+            for (i in dealer_cards) {
                 worth += give_worth_of_the_card(i)
             }
             worth
         }
 
         dealer.text = dealer_cards[0] + " X"
-        player.text = player_cards[0] + " "+ player_cards[1]
+        player.text = player_cards[0] + " " + player_cards[1]
 
         d_status.text = give_worth_of_the_card(dealer_cards[0]).toString()
         p_status.text = (give_worth_of_the_card(player_cards[0]) + give_worth_of_the_card(player_cards[1])).toString()
 
-        p_status.text = "Bet: "+ bet.toString() + " -- " + p_status.text.toString()
+        p_status.text = "Bet: " + bet.toString() + " -- " + p_status.text.toString()
 
-        if(dc_worth == 21) {//dealer blackjack
+        if (dc_worth == 21) {//dealer blackjack
             game_over(dc_worth, pl_worth, 0)
-        }else if(dc_worth > 21){
+        } else if (dc_worth > 21) {
             //dealer busted
             game_over(dc_worth, pl_worth, 1)
-        }else {
+        } else {
         }
     }
 
-    fun hit(){
+    fun hit() {
         double_b.isEnabled = false
         dealer_cards.add(random_card())
         player_cards.add(random_card())
@@ -144,13 +147,13 @@ class MainActivity : AppCompatActivity() {
             game_over(dc_w, pl_w, 1)
         } else if (dc_w >= 17) {
             game_over(dc_w, pl_w, 3)
-        }else if(pl_w >= 21){
+        } else if (pl_w >= 21) {
             game_over(dc_w, pl_w, 3)
         }
         /// HIT
     }
 
-    fun stand(){
+    fun stand() {
         stand.isEnabled = false
         double_b.isEnabled = false
         while (dc_w < 17) {
@@ -169,17 +172,17 @@ class MainActivity : AppCompatActivity() {
                 game_over(dc_w, pl_w, 0)
             } else if (dc_w > 21) {//dealer busted
                 game_over(dc_w, pl_w, 1)
-            }else if(dc_w >= 17){
+            } else if (dc_w >= 17) {
                 game_over(dc_w, pl_w, 3)
-            }else if(pl_w >= 21){
-            game_over(dc_w, pl_w, 3)
-        }
+            } else if (pl_w >= 21) {
+                game_over(dc_w, pl_w, 3)
+            }
         }
 
     }
 
-    fun double_b(){
-        if(player_cards.size < 3){
+    fun double_b() {
+        if (player_cards.size < 3) {
 
             stand.isEnabled = false
             double_b.isEnabled = false
@@ -221,59 +224,70 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun random_card(): String{
-        return cards[Random.nextInt(0,13)]
+    fun random_card(): String {
+        return cards[Random.nextInt(0, 13)]
     }
 
-    fun give_worth_of_the_card(card: String): Int{
-        if(card != "A" && card != "J" && card != "Q" && card != "K"){
+    fun give_worth_of_the_card(card: String): Int {
+        if (card != "A" && card != "J" && card != "Q" && card != "K") {
             return card.toInt()
-        }else{
-            if(card != "A"){
+        } else {
+            if (card != "A") {
                 return 10
-            }else {
+            } else {
                 return 1
             }
         }
 
     }
 
-    fun game_over(dc:Int, pc:Int, result: Int){
+    fun game_over(dc: Int, pc: Int, result: Int) {
         var status: String = ""
-        if(result == 3) {
+        if (result == 3) {
             if (pc == 21) {
                 status = "Player blackjack. You won!"
-            } else if (pc > 21) status = "Player lost!"
-            else if (dc < pc) {
+                wallet += bet * 2
+            } else if (pc > 21) {
+                status = "Player lost!"
+            } else if (dc < pc) {
                 status = "Player won!"
-            } else if (dc == pc) status = "Push. No one won"
-            else status = "Dealer won!"
-        }else if(result == 4) {
+                wallet += bet * 2
+            } else if (dc == pc) {
+                status = "Push. No one won"
+                wallet += bet
+            } else status = "Dealer won!"
+        } else if (result == 4) {
             status = "Player busted! You lost"
+        } else if (result == 0) {
+            status = "Dealer Blackjack. You lost!"
+        } else if (result == 1) {
+            status = "Dealer Busted. You Won"
+            wallet += bet * 2
         }
 
-
+        status += " You have $" + wallet.toString()
         val builder = AlertDialog.Builder(this)
-        builder.setTitle( if(result == 0) "Dealer Blackjack. You lost!" else if(result==1) "Dealer Busted. You Won"
-        else status )
-        builder.setMessage("New Game?")
+        builder.setTitle(status)
+        builder.setMessage(if (wallet > 0) "New Game?" else "Thanks for playing. You are poor now!")
         //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
 
         builder.setPositiveButton(android.R.string.yes) { dialog, which ->
-            Toast.makeText(applicationContext,
-                android.R.string.yes, Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                applicationContext,
+                android.R.string.yes, Toast.LENGTH_SHORT
+            ).show()
         }
 
         builder.setNegativeButton(android.R.string.no) { dialog, which ->
-            Toast.makeText(applicationContext,
-                android.R.string.no, Toast.LENGTH_SHORT).show()
+            exitProcess(-1)
         }
+        if (wallet > 0) {
+            builder.setNeutralButton("New Game") { dialog, which ->
+                stand.isEnabled = true
+                double_b.isEnabled = true
+                enter_bet()
 
-        builder.setNeutralButton("New Game") { dialog, which ->
-
-            stand.isEnabled = true
-            double_b.isEnabled = true
-            enter_bet()
+            }
         }
         builder.show()
     }
